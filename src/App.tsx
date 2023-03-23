1e8b2c13015a51beb8d89bccc19bba5caff06b41
Constance Wang <constancezwang@gmail.com>
2023-03-23 12:53:16 -0400

    prompts

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Space,
+  Typography,
+  Col,
+  Row,
+  Form,
+  Input,
+  Button,
+  Image,
+  Grid,
+} from "antd";
+import { examples } from "./examples";
+
+const { useBreakpoint } = Grid;
+const { Title } = Typography;
+
+function PlaygroundForm() {
+  return (
+    <Form>
+      <Form.Item>
+        <Input.TextArea
+          placeholder="Enter your prompt"
+          style={{ height: 240 }}
+          showCount
+          maxLength={600}
+        />
+      </Form.Item>
+      {/* <Form.Item>
+        <Input.TextArea
+          placeholder="Enter a negative prompt"
+          showCount
+          maxLength={100}
+        />
+      </Form.Item> */}
+      <Form.Item>
+        <Button type="primary" size="large">
+          Generate Image
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+}
+
+function Playground() {
+  return (
+    <Space direction="vertical" style={{ width: "100%" }} size="small">
+      <Title level={3}>Playground</Title>
+      Just enter your prompt and click the generate button.
+      <PlaygroundForm />
+    </Space>
+  );
+}
+
+function Example() {
+  const {prompt, negativePrompt, image} = examples[1];
+
+  return (
+    <Space direction="vertical" style={{ width: "100%" }} size="small">
+      <Title level={3}>Example</Title>
+      <strong>Prompt: </strong>{prompt}
+      <strong>Negative prompt:</strong>{negativePrompt}
+      <strong>Generated:</strong>
+      <Image width={300} src={image} preview={false}/>
+    </Space>
+  );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const { md } = useBreakpoint();
+
+  return ( md ?
+    <Row style={{ margin: 24 }}>
+      <Col span={12} style={{ padding: 24 }}>
+        <Playground />
+      </Col>
+      <Col span={12} style={{ padding: 24 }}>
+        <Example />
+      </Col>
+    </Row>
+  : <Space direction="vertical" style={{width: "100%"}}>
+    <Example/>
+    <Playground/>
+  </Space>);
 }
 
 export default App;
---
+++ src/examples.ts
@@ -0,0 +1,17 @@
+export const examples = [
+  {
+    prompt: `Here I am noble; I am boyar; the common people know me, and I am master. But a stranger in a strange land, he is no one; men know him not—and to know not is to care not for. I am content if I am like the rest, so that no man stops if he see me, or pause in his speaking if he hear my words, 'Ha, ha! a stranger!`,
+    negativePrompt: "none",
+    image: "test-1.jfif",
+  },
+  {
+    prompt: `Everything about vaporwave is tied to capitalist sleaze; even its name is a spoof of the term "vaporware," nonexistent products that companies announce and heavily promote as a corporate strategy to keep their competitors at bay  It has been described "a degrading of commercial music" in an attempt to reveal the "false promises" of capitalism. Unlike seapunk, vaporwave is actually "punk," in that it's driven by a subversive political objective: undermining the iron grip of global capitalism... by exposing the alienating emptiness underneath its uncanny sheen.  Vaporwave is anti-capitalist Aesthetics`,
+    negativePrompt: "quote",
+    image: "test-2-1.jfif",
+  },
+  {
+    prompt: `They were on the crest of a hill. The sun had set some time since, but the landscape was still clear in the mellow afterlight. To the west a dark church spire rose up against a marigold sky. Below was a little valley and beyond a long, gently-rising slope with snug farmsteads scattered along it. From one to another the child’s eyes darted, eager and wistful. At last they lingered on one away to the left, far back from the road, dimly white with blossoming trees in the twilight of the surrounding woods. Over it, in the stainless southwest sky, a great crystal-white star was shining like a lamp of guidance and promise.`,
+    negativePrompt: "none",
+    image: "test-2-2.jfif",
+  }
+];
